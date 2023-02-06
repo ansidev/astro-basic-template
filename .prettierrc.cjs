536e113283da406ccc3c124b25f070a5b6088d77
Le Minh Tri <ansidev@gmail.com>
2023-02-06 19:15:10 +0700

    feat(config-prettier): add prettier configs

---
+++ .prettierrc.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  plugins: [require.resolve('prettier-plugin-astro')],
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+      },
+    },
+  ],
+}
