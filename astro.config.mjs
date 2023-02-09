06c077f397dabd116e3e1ef28351eb98d5833737
Le Minh Tri <ansidev@gmail.com>
2023-02-09 07:41:09 +0700

    build: remove unused css from build output

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from 'astro/config'
 import compress from 'astro-compress'
+import purgecss from 'astro-purgecss'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [compress()]
+  integrations: [
+    purgecss(),
+    compress(),
+  ]
 })
